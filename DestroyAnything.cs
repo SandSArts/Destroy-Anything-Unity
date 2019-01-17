f5b1f66050529c1ffcb5c9479cecc3602d15adbf
SandS Arts <sandsarts.developer@gmail.com>
2019-01-17 02:09:13 -0800

    Add files via upload

---
+++ DestroyAnything.cs
@@ -0,0 +1,22 @@
+﻿//Programmed by Shashank.S - creator of SandS Arts , Mail me at sandsarts.developer@gmail.com
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAnything : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      Destroy(this.GetComponent<BoxCollider>(),2f);    //Destroying BoxCollider attached to this game object after 2 seconds
+      Destroy(this,3f);                                //Destroying this script after 3 seconds
+      Destroy(this.GetComponent<ScriptName>());        //Destroying Script called ScriptName attached to this game object immediately after hitting play
+      Destroy(this.gameObject);        //Destroying this game object immediately after hitting play
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
